71c0c901afa398d8258007ae4999316e3c994191
Deekshitha V <133587688+Deeksha27Deekshitha@users.noreply.github.com>
2025-01-09 18:45:55 +0530

    Update accordion.js

--- src/utils/accordion.js
+++ src/utils/accordion.js
@@ -4,18 +4,18 @@ import {MdCancel, MdAnalytics} from "react-icons/md";
 const data=[
     {
         icon:<HiShieldCheck/>,
-        heading:"Best interrest rates on the market",
-        detail:"Exercitation in fugiat est ut ad ea cupidatat occaecat consequat est minim minimm esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+        heading:"Market-Leading Interest Rates",
+        detail:"We provide market-leading interest rates, making your property investment more affordable and ensuring maximum value for your future",
     },
     {
         icon:<MdCancel/>,
         heading:"Prevent unstable price",
-        detail:"Exercitation in fugiat est ut ad ea cupidatat occaecat consequat est minim minimm esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+        detail:"We protect you from price instability, ensuring you make secure investments with predictable long-term value",
     },
     {
         icon:<MdAnalytics/>,
         heading:"Best price in the market",
-        detail:"Exercitation in fugiat est ut ad ea cupidatat occaecat consequat est minim minimm esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+        detail:"We offer the best prices in the market, ensuring you get exceptional value for your property investment",
     },
 ];
 export default data;
